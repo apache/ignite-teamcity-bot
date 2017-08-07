0aad75d7cf24de45888b3e5bfc9c9743d4d924df
dpavlov <dpavlov@gridgain.com>
2017-08-07 19:58:31 +0300

    muted tests metrics collection

--- src/main/java/org/apache/ignite/ci/db/TcHelperDb.java
+++ src/main/java/org/apache/ignite/ci/db/TcHelperDb.java
@@ -2,12 +2,19 @@ package org.apache.ignite.ci.db;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Collection;
+import java.util.Collections;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.ci.HelperConfig;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.configuration.PersistentStoreConfiguration;
 import org.apache.ignite.logger.java.JavaLogger;
+import org.apache.ignite.spi.IgniteSpiContext;
+import org.apache.ignite.spi.IgniteSpiException;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.TcpDiscoveryIpFinder;
 
 /**
  * Created by dpavlov on 04.08.2017
@@ -17,6 +24,14 @@ public class TcHelperDb {
     public static Ignite start() {
         final IgniteConfiguration cfg = new IgniteConfiguration();
         setWork(cfg, HelperConfig.resolveWorkDir());
+
+        TcpDiscoverySpi spi = new TcpDiscoverySpi();
+        int locPort = 10000;
+        spi.setLocalPort(locPort);
+        spi.setLocalPortRange(locPort);
+        spi.setIpFinder(new LocalOnlyTcpDiscoveryIpFinder(locPort));
+
+        cfg.setDiscoverySpi(spi);
         cfg.setConsistentId("TcHelper");
         cfg.setGridLogger(new JavaLogger());
 
@@ -40,5 +55,46 @@ public class TcHelperDb {
 
     public static void stop(Ignite ignite) {
         Ignition.stop(ignite.name(), false);
+    }
+
+    private static class LocalOnlyTcpDiscoveryIpFinder implements TcpDiscoveryIpFinder {
+        private int port;
+
+        public LocalOnlyTcpDiscoveryIpFinder(int port) {
+            this.port = port;
+        }
+
+        @Override public void onSpiContextInitialized(IgniteSpiContext spiCtx) throws IgniteSpiException {
+
+        }
+
+        @Override public void onSpiContextDestroyed() {
+
+        }
+
+        @Override
+        public void initializeLocalAddresses(Collection<InetSocketAddress> addrs) throws IgniteSpiException {
+
+        }
+
+        @Override public Collection<InetSocketAddress> getRegisteredAddresses() throws IgniteSpiException {
+            return Collections.singletonList(new InetSocketAddress("localhost", port));
+        }
+
+        @Override public boolean isShared() {
+            return false;
+        }
+
+        @Override public void registerAddresses(Collection<InetSocketAddress> addrs) throws IgniteSpiException {
+
+        }
+
+        @Override public void unregisterAddresses(Collection<InetSocketAddress> addrs) throws IgniteSpiException {
+
+        }
+
+        @Override public void close() {
+
+        }
     }
 }
