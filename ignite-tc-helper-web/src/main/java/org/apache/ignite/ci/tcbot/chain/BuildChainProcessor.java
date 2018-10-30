58107fecc1a6d1ef94a94e6b4a73ed21c16aa392
Dmitriy Pavlov <dpavlov@apache.org>
2018-10-30 14:05:17 +0300

    IGNITE-9848: Loading builds chain in accordance with fat build approach, calculate statistics moved to later execute.

--- ignite-tc-helper-web/src/main/java/org/apache/ignite/ci/tcbot/chain/BuildChainProcessor.java
+++ ignite-tc-helper-web/src/main/java/org/apache/ignite/ci/tcbot/chain/BuildChainProcessor.java
@@ -252,10 +252,12 @@ public class BuildChainProcessor {
 
     @SuppressWarnings("WeakerAccess")
     @AutoProfiling
-    protected static void analyzeTests(MultBuildRunCtx outCtx, IAnalyticsEnabledTeamcity teamcity,
+    protected void analyzeTests(MultBuildRunCtx outCtx, IAnalyticsEnabledTeamcity teamcity,
                                      ProcessLogsMode procLog) {
         for (SingleBuildRunCtx ctx : outCtx.getBuilds()) {
-            teamcity.calculateBuildStatistic(ctx);
+            tcUpdatePool.getService().submit(() -> {
+                teamcity.calculateBuildStatistic(ctx);
+            });
 
             if ((procLog == ProcessLogsMode.SUITE_NOT_COMPLETE && ctx.hasSuiteIncompleteFailure())
                     || procLog == ProcessLogsMode.ALL)
