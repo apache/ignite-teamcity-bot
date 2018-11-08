2932064ce0bc572f8bf5c3931273b39258a6739d
Дмитрий Рябов <SomeFireOne@gmail.com>
2018-11-09 00:46:47 +0300

    Bugfix

--- ignite-tc-helper-web/src/main/webapp/js/testfails-2.1.js
+++ ignite-tc-helper-web/src/main/webapp/js/testfails-2.1.js
@@ -693,11 +693,11 @@ function showSuiteData(suite, settings) {
  * @returns {boolean} True - if test is new. False - otherwise.
  */
 function isNewFailedTest(testFail) {
-    if (!isDefinedAndFilled(testFail.histBaseBranch) || !isDefinedAndFilled(testFail.histBaseBranch.latestRuns))
-        return true;
-
     if (isDefinedAndFilled(testFail.webIssueUrl))
         return false;
+
+    if (!isDefinedAndFilled(testFail.histBaseBranch) || !isDefinedAndFilled(testFail.histBaseBranch.latestRuns))
+        return true;
 
     var hist = testFail.histBaseBranch;
 
