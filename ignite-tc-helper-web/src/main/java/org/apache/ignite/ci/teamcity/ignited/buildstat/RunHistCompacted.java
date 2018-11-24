3c025577572eb044f5e180ab90a26e6777d1616d
Dmitriy Pavlov <dpavlov@apache.org>
2018-11-24 18:06:33 +0300

    IGNITE-9542: New Run stripe implementation: skeleton for new entities

---
+++ ignite-tc-helper-web/src/main/java/org/apache/ignite/ci/teamcity/ignited/buildstat/RunHistCompacted.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.ci.teamcity.ignited.buildstat;
+
+import org.apache.ignite.cache.query.annotations.QuerySqlField;
+import org.apache.ignite.ci.analysis.IVersionedEntity;
+
+public class RunHistCompacted implements IVersionedEntity {
+    /** Latest version. */
+    private static final int LATEST_VERSION = 1;
+
+    /** Entity fields version. */
+    @SuppressWarnings("FieldCanBeLocal")
+    private short _ver = LATEST_VERSION;
+
+    @QuerySqlField(orderedGroups = {@QuerySqlField.Group(name = "tstAndSrv", order = 0)})
+    private int testNameOrSuite;
+
+    @QuerySqlField(orderedGroups = {@QuerySqlField.Group(name = "tstAndSrv", order = 1)})
+    private int srvId;
+
+    /**
+     * Runs registered all the times.
+     */
+    private int runs;
+
+    /**
+     * Failures registered all the times.
+     */
+    private int failures;
+
+
+    /** {@inheritDoc} */
+    @Override public int version() {
+        return _ver;
+    }
+
+    /** {@inheritDoc} */
+    @Override public int latestVersion() {
+        return LATEST_VERSION;
+    }
+}
---
+++ ignite-tc-helper-web/src/main/java/org/apache/ignite/ci/teamcity/ignited/buildstat/RunHistCompactedDao.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.ci.teamcity.ignited.buildstat;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.cache.QueryEntity;
+import org.apache.ignite.ci.db.TcHelperDb;
+import org.apache.ignite.ci.teamcity.ignited.IStringCompactor;
+import org.apache.ignite.ci.teamcity.ignited.IgniteStringCompactor;
+import org.apache.ignite.configuration.CacheConfiguration;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import java.util.Collections;
+
+public class RunHistCompactedDao {
+    /** Cache name*/
+    public static final String TEST_HIST_CACHE_NAME = "testRunHist";
+
+    /** Ignite provider. */
+    @Inject
+    private Provider<Ignite> igniteProvider;
+
+    /** Test history cache. */
+    private IgniteCache<RunHistKey, RunHistCompacted> testHistCache;
+
+    /** Build start time. */
+    private IgniteCache<Integer, Long> buildStartTime;
+
+    /** Compactor. */
+    @Inject private IStringCompactor compactor;
+
+    /**
+     * Initialize
+     */
+    public void init () {
+        Ignite ignite = igniteProvider.get();
+
+        final CacheConfiguration<RunHistKey, RunHistCompacted> cfg = TcHelperDb.getCacheV2Config(TEST_HIST_CACHE_NAME);
+
+        cfg.setQueryEntities(Collections.singletonList(new QueryEntity(RunHistKey.class, RunHistCompacted.class)));
+
+        testHistCache = ignite.getOrCreateCache(cfg);
+    }
+}
