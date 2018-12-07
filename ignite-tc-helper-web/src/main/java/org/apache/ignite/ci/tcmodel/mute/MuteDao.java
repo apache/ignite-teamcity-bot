9572a79f9fb1b1bf83700975870183a664e54cd2
Nikolai Kulagin <31161239+zzzadruga@users.noreply.github.com>
2018-12-07 12:09:10 +0300

    Update ignite-tc-helper-web/src/main/java/org/apache/ignite/ci/tcmodel/mute/MuteDao.java
    Co-Authored-By: SomeFire <SomeFireOne@gmail.com>

--- ignite-tc-helper-web/src/main/java/org/apache/ignite/ci/tcmodel/mute/MuteDao.java
+++ ignite-tc-helper-web/src/main/java/org/apache/ignite/ci/tcmodel/mute/MuteDao.java
@@ -18,7 +18,6 @@
 package org.apache.ignite.ci.tcmodel.mute;
 
 import com.google.common.base.Preconditions;
-import java.util.HashSet;
 import java.util.Set;
 import javax.inject.Inject;
 import javax.inject.Provider;
