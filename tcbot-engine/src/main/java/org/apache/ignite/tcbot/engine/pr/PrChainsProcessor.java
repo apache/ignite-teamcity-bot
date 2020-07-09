177bd1e7bcf0b7fc3290328e249e5b68d73a8de2
sergeyuttsel <uttsel@gmail.com>
2020-07-09 23:16:13 +0300

    New tests list doesn't contain ignored and muted tests. Fix visa text. #169
    Signed-off-by: Ivan Rakov <ivan.glukos@gmail.com>

--- tcbot-engine/src/main/java/org/apache/ignite/tcbot/engine/pr/PrChainsProcessor.java
+++ tcbot-engine/src/main/java/org/apache/ignite/tcbot/engine/pr/PrChainsProcessor.java
@@ -393,7 +393,7 @@ public class PrChainsProcessor {
             .map((ctx) -> {
                 List<ShortTestUi> missingTests = ctx.getFilteredTests(test -> {
                     IRunHistory history = test.history(tcIgnited, baseBranchId, null);
-                    return history == null;
+                    return history == null && !test.isMutedOrIgored();
                 })
                     .stream()
                     .map(occurrence -> new ShortTestUi().initFrom(occurrence, occurrence.isPassed()))
